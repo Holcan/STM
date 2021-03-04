4eba4c704ea258c5c3771b9978dc56b3ce740ecd
Alejandro Hernández de la Vega <ahv@ciencias.unam.mx>
2021-03-04 15:47:47 +0100

    cleaned notebook
    cleaned notebook and changed some variables in the sweeping python script to avoid confusion

--- M8190/Documentation/Sweeping.py
+++ M8190/Documentation/Sweeping.py
@@ -1,60 +1,60 @@
 import numpy as np
 
-def sweep0(Pulse,P,p,t):
+def sweep0(Pulse,N,n,t):
     
     """Sweeping function for a single pulse with respect of time and duration.
-
+    
     This function takes the Amplitude, Start and End time, Start and End and Duration of the Pulse dictionary.
-    Given a total number of points P, it calcultes the time step dt and the duration step dτ depending on 
+    Given a total number of points N, it calcultes the time step dt and the duration step dτ depending on 
     the values of 'Sweep Duration' and 'Sweep time' keys. 
-    It then calculates the pulse shape up to the given p point at a time t.
+    It then calculates the pulse shape up to the given n point at a time t.
     
     If both of the sweeping keys are equal to zero, it just gives the puls shape at 'Starting time' with 'Starting Duration' at     time t
     """
     
-    if 0 <= p <= P:
+    if 0 <= n <= N:
         
         if Pulse['Sweep time'] == 0:
             dt = 0
 
         #Sweeping with respect to time
         if Pulse['Sweep time'] == 1:
-            dt = (Pulse['End time'] - Pulse['Start time'])/P
+            dt = (Pulse['End time'] - Pulse['Start time'])/N
 
         if Pulse['Sweep Duration'] == 0:
             dτ = 0
 
         #Sweeping with respect to Duration
         if Pulse['Sweep Duration'] == 1:
-            dτ = (Pulse['End Duration'] - Pulse['Start Duration'])/P
+            dτ = (Pulse['End Duration'] - Pulse['Start Duration'])/N
 
 
-        new_start = p * dt + Pulse['Start time']
-        new_duration = p * dτ + Pulse['Start Duration']
+        new_start = n * dt + Pulse['Start time']
+        new_duration = n * dτ + Pulse['Start Duration']
         
         pol = Pulse['Amplitude'] * (new_start <= t <= new_duration + new_start)
         return pol
     
     else:
-        print('p must be in the interval',[0,P])
+        print('n must be in the interval',[0,N])
         
-def Sweep(PulList,P,p,t,N):
+def Sweep(PulList,N,n,t,Δt):
     
     """Function that perfoms or not a Sweep, depending on the Sweep dictionary keys, for the Pulse Scheme PulList.
     
-    This function is a generalization of the sweep0(Pulse,P,p,t).
+    This function is a generalization of the sweep0(Pulse,N,n,t).
     It calls it and maps it over every Pulse in the list of pulses PulList.
     If two pulses overlap, their amplitude is added up.
-    P gives the number of sample points for the overal pulse scheme, not to be confuced with dt, the sweeping time step given by P
+    Δt gives the number of time steps for the overal pulse scheme, not to be confuced with dt, the sweeping time step given by N
     """
     
     #time interval
-    time = np.linspace(-1e-10,t,N)
+    time = np.linspace(-1e-10,t,Δt)
     #Each pulse will be an array, and an entry of a bigger array (we have an array of arrays)
     pularray = np.zeros((len(PulList),len(time)))
     
     for i in range(0,len(PulList)):
-        pularray[i] = np.array([sweep0(PulList[i],P,p,x) for x in time])
+        pularray[i] = np.array([sweep0(PulList[i],N,n,x) for x in time])
         
     #the final pulse scheme will be the overlap of each individual pulse, given by the sum of their arrays    
     pulscheme = np.zeros(len(pularray[0]))
