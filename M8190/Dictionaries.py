749896484456ba8708438c4837f833b0589da035
Alejandro Hernández de la Vega <ahv@ciencias.unam.mx>
2021-03-05 16:00:26 +0100

    Parameter function, Dictionarie Modules
    Added the parameter calculating function for the proper pulse, given desired time length and resolution. Also moved every dictionary to a module for cleaner presentation.

---
+++ M8190/Dictionaries.py
@@ -0,0 +1,67 @@
+'Here are the Dictionaries that correspond to the Pulses and the AWG settings'
+'Time is given in $\mu$seconds, Amplitude is Voltage in milivolts.'
+
+
+M8190A_S1={
+    'Visa_Resource_Name': 'TCPIP0::ibn3-036.ibn-net.kfa-juelich.de::hislip0::INSTR',
+    'Amplitude':300,
+    'Clock Sample Frecuency':925000000,
+    'Output_Channel': 1,
+    'Output_rout': 'DC'
+
+}
+
+Rabi={
+    'Name':'Rabi',
+    'Amplitude':200,
+    'Start time':1e-10,
+    'End time':2e-10,
+    'Start Duration':5,
+    'End Duration':6,
+    'Sweep time':0,
+    'Sweep Duration':0
+}
+
+Probe1={
+    'Name':'Probe1',
+    'Amplitude':50,
+    'Start time':6,
+    'End time':20,
+    'Start Duration':7,
+    'End Duration':9 ,
+    'Sweep time':1,
+    'Sweep Duration':0
+}
+
+Rabi2={
+    'Name':'Rabi2',
+    'Amplitude':-200,
+    'Start time':1e-10,
+    'End time':2e-10,
+    'Start Duration':5,
+    'End Duration':6,
+    'Sweep time':0,
+    'Sweep Duration':0
+}
+
+Probe3={
+    'Name':'Probe3',
+    'Amplitude':5,
+    'Start time':5,
+    'End time':10,
+    'Start Duration':1,
+    'End Duration':5,
+    'Sweep time':1,
+    'Sweep Duration':0
+}
+
+PulsoG={
+    'Name':'PulsoG',
+    'Amplitude':200,
+    'Start time':1e-10,
+    'End time':9,
+    'Start Duration':7,
+    'End Duration':6 ,
+    'Sweep time':0,
+    'Sweep Duration':0
+}
