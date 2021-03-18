48748aac81de29db39d035dd65790f9aa7a82953
Alejandro Hernández de la Vega <ahv@ciencias.unam.mx>
2021-03-18 16:30:14 +0100

    Seq problem.
    Sampling frequency has some effect with the segmentation for the sequences, changing it from 125Mhz to 1Ghz made it so 6 repetitions of the segments transform into 3. Still have to play with this.
    
    tomorrow: do a run with the 7.2 GHz sample rate and see if I get individual segments per sequences, as desired

--- M8190/Dictionaries.py
+++ M8190/Dictionaries.py
@@ -5,6 +5,15 @@
 AWG_S1={
     'Visa_Resource_Name': 'TCPIP0::ibn3-036.ibn-net.kfa-juelich.de::hislip0::INSTR',
     'Amplitude':300,
+    'Clock Sample Frecuency':125000000,
+    'Output_Channel': 1,
+    'Output_rout': 'DC'
+
+}
+
+AWG_S2={
+    'Visa_Resource_Name': 'TCPIP0::ibn3-036.ibn-net.kfa-juelich.de::hislip0::INSTR',
+    'Amplitude':300,
     'Clock Sample Frecuency':7200000000,
     'Output_Channel': 1,
     'Output_rout': 'DC'
@@ -16,7 +25,7 @@ Rabi={
     'Amplitude':200,
     'Start time':1e-10,
     'End time':2e-10,
-    'Start Duration':5,
+    'Start Duration':2,
     'End Duration':6,
     'Sweep time':0,
     'Sweep Duration':0
@@ -25,9 +34,9 @@ Rabi={
 Probe1={
     'Name':'Probe1',
     'Amplitude':50,
-    'Start time':6,
+    'Start time':2,
     'End time':20,
-    'Start Duration':7,
+    'Start Duration':2,
     'End Duration':9 ,
     'Sweep time':1,
     'Sweep Duration':0
@@ -35,33 +44,44 @@ Probe1={
 
 Rabi2={
     'Name':'Rabi2',
-    'Amplitude':-200,
-    'Start time':1e-10,
+    'Amplitude':-100,
+    'Start time':1,
     'End time':2e-10,
-    'Start Duration':5,
+    'Start Duration':2,
     'End Duration':6,
     'Sweep time':0,
     'Sweep Duration':0
 }
 
-Probe3={
-    'Name':'Probe3',
-    'Amplitude':5,
-    'Start time':5,
-    'End time':10,
-    'Start Duration':1,
-    'End Duration':5,
+Probe2={
+    'Name':'Probe2',
+    'Amplitude':-50,
+    'Start time':2,
+    'End time':20,
+    'Start Duration':2,
+    'End Duration':9,
     'Sweep time':1,
     'Sweep Duration':0
 }
 
 PulsoG={
     'Name':'PulsoG',
-    'Amplitude':200,
-    'Start time':1e-10,
-    'End time':9,
-    'Start Duration':7,
-    'End Duration':6 ,
-    'Sweep time':0,
+    'Amplitude':-50,
+    'Start time':2,
+    'End time':20,
+    'Start Duration':1,
+    'End Duration':9,
+    'Sweep time':1,
+    'Sweep Duration':0
+}
+
+PulsoH={
+    'Name':'PulsoH',
+    'Amplitude':-50,
+    'Start time':3,
+    'End time':20,
+    'Start Duration':1,
+    'End Duration':9,
+    'Sweep time':1,
     'Sweep Duration':0
 }
